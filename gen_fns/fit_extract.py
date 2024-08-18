b272986e968eadfde4708f646940c823ed0967d8
Jessica Stewart <n9953523@qut.edu.au>
2024-08-18 22:01:12 +1000

    added more comments

--- gen_fns/fit_extract.py
+++ gen_fns/fit_extract.py
@@ -3,13 +3,14 @@
 # ----- Imports -----
 
 import os
-import zipfile
 import pandas as pd
 import matplotlib.pyplot as plt
 
-cdfs = pd.read_csv(os.path.join('/', 'Users', 'jess', 'Desktop', 'git_python', 'git_data', 'cdfs.csv'))
-cos = pd.read_csv(os.path.join('/', 'Users', 'jess', 'Desktop', 'git_python', 'git_data', 'cos.csv'))
-uds = pd.read_csv(os.path.join('/', 'Users', 'jess', 'Desktop', 'git_python', 'git_data', 'uds.csv'))
+# [Adjust File Paths as Necessary]
+
+cdfs = pd.read_csv(os.path.join('/', 'Users', 'jess', 'Desktop', 'git_mphil', 'git_data', 'cdfs.csv'))
+cos = pd.read_csv(os.path.join('/', 'Users', 'jess', 'Desktop', 'git_mphil', 'git_data', 'cos.csv'))
+uds = pd.read_csv(os.path.join('/', 'Users', 'jess', 'Desktop', 'git_mphil', 'git_data', 'uds.csv'))
 
 
 # ----- Extract Data -----
@@ -66,14 +67,16 @@ def plot_fit_files(field, ids):
     
     for source_id in ids:
 
+        # [Adjust File Paths as Necessary]
+
         if field == 'cdfs':
-            file_path = f'/Users/jess/Desktop/git_mphil/spectra/{field}_fast.zip/{field}.v1.6.9_{source_id}.fit'
+            file_path = f'/Users/jess/Desktop/git_spectra/{field}_fast/{field}.v1.6.9_{source_id}.fit'
 
         elif field == 'cosmos':
-            file_path = f'/Users/jess/Desktop/git_mphil/spectra/{field}_fast.zip/{field}.v1.3.6_{source_id}.fit'
+            file_path = f'/Users/jess/Desktop/git_spectra/{field}_fast/{field}.v1.3.6_{source_id}.fit'
 
         elif field == 'uds':
-            file_path = f'/Users/jess/Desktop/git_mphil/spectra/{field}_fast.zip/{field}.v1.5.8_{source_id}.fit'
+            file_path = f'/Users/jess/Desktop/git_spectra/{field}_fast/{field}.v1.5.8_{source_id}.fit'
 
         else:
 
@@ -90,31 +93,45 @@ def plot_fit_files(field, ids):
             wavelengths = [row[0] for row in data]
             fluxes = [row[1] for row in data]
 
-            plt.plot(wavelengths, fluxes, label=f'ID {source_id}')
+            plt.plot(wavelengths, fluxes, label = f'ID: {source_id}')
     
     # -- Plot Aesthetics --
             
+    # plt.title(f'Select Spectra from {field.upper()}')
+                    
     plt.xlabel('Wavelength (Angstrom)')
     plt.ylabel('Flux (x 10^-19 ergs s^-1 cm^-2 Angstrom^-1)')
-    plt.title(f'Spectra for {field.upper()} Field')
+
+    plt.text(0.95, 0.05, f'{field.upper()}', fontsize = 13, color = 'k', ha = 'right', va = 'bottom', transform = plt.gca().transAxes)
 
     plt.xlim(0, 60000)
-    plt.ylim(0, 650)
-    plt.legend()
+    plt.ylim(bottom = 0)
 
-    plt.savefig('/Users/jess/Desktop/git_data/outputs/fast/xray_rad.png', bbox_inches = 'tight', dpi = 300, facecolor = 'white', transparent = False)
+    handles, labels = plt.gca().get_legend_handles_labels()
+
+    plt.legend(handles[:10], labels[:10])       # [Adjust no. of labels as necessary, default cap is 10 to prevent overcrowding]
+
+    # [Adjust Save Path as Necessary]
+
+    plt.savefig('/Users/jess/Desktop/git_mphil/outputs/fast/agn_uds.png', bbox_inches = 'tight', dpi = 300, facecolor = 'white', transparent = False)
 
     plt.show()
 
-# ----- Produce Masked Array -----
+
+
+# ----- Dynamic Parameters -----
+    
+# -- Output Masked Array (Optional) --
     
 for df in [cdfs, cos, uds]:
 
-    mask = ((df['xray_agn'] == 1)) 
-    id_array = df.loc[mask, 'id'].values
+    mask = ((df['agn'] == 1))                # [Define desired mask condition(s)]
 
-# ----- Notable Source IDs (COSMOS) -----
+    id_array = df.loc[mask, 'id'].values        # Extracts IDs of sources that meet mask conditions for plot
 
-field_name = 'cosmos'  # Can be 'cdfs', 'cosmos', or 'uds'
-source_ids = id_array  # Replace with your actual source IDs
-plot_fit_files(field_name, source_ids)
+# -- Field Name & Source IDs --
+
+field_name = 'uds'                             # [Input desired field: Can be 'cdfs', 'cosmos', or 'uds']
+source_ids = id_array                           # [Input desired ID array]
+
+plot_fit_files(field_name, source_ids)          # Calls plot function
